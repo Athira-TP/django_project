20d87b195e2c430c147beb11676bf83a8424e7f4
athiratp <athira.tp@yougotagift.com>
2023-12-27 14:11:26 +0530

    removed some code

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,27 +1,8 @@
 from django.urls import path, include
 from snippets import views
-
 from rest_framework.routers import DefaultRouter
 
 
-# snippet_list = SnippetViewSet.as_view({
-#     'get': 'list',
-#     'post': 'create'
-# })
-# snippet_detail = SnippetViewSet.as_view({
-#     'get': 'retrieve',
-#     'put': 'update',
-#     'delete': 'destroy'
-# })
-# snippet_highlight = SnippetViewSet.as_view({
-#     'get': 'highlight'
-# }, renderer_classes=[renderers.StaticHTMLRenderer])
-# user_list = UserViewSet.as_view({
-#     'get': 'list'
-# })
-# user_detail = UserViewSet.as_view({
-#     'get': 'retrieve'
-# })
 router = DefaultRouter()
 router.register(r'snippets', views.SnippetViewSet, basename='snippet')
 router.register(r'users', views.UserViewSet, basename='user')
